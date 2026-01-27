fc8ced308af21b2c996a5df5df522bd1da512e6a
Chance Krueger <chancekrueger@arizona.edu>
2026-01-27 14:42:20 -0700

    Initial Commit

---
+++ Chance Budget Insights/Chance_Budget_InsightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chance_Budget_InsightsApp.swift
+//  Chance Budget Insights
+//
+//  Created by Chance Krueger on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Chance_Budget_InsightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chance Budget Insights/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chance Budget Insights
+//
+//  Created by Chance Krueger on 1/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
